0832316e43c136d00f27b898564758a940da8d03
Veka <veka61@laposte.net>
2018-04-15 10:49:49 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,12 +9,17 @@ echoVERT ':: Mise a jour du serveur'
 apt-get -y update
 apt-get -y upgrade
 
-echoVERT ':: Ajout du depot'
+echoVERT ':: Ajout des librairies'
 apt-get -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+
+echoVERT ':: Ajout du depot officiel de docker'
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - 
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+
+echoVERT ':: Mise a jour des depots'
+apt-get -y update
 
 echoVERT ':: Installation de Docker-CE'
-apt-get -y update
 apt-get -y install docker-ce
 
 echoVERT ':: Fin du script'
