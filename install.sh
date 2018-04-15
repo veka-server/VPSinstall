87ad8f13b1a58fc06e3a905427422770077b9dce
Veka <veka61@laposte.net>
2018-04-15 10:15:34 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,16 +5,16 @@ echoVERT()
     echo -e "\033[31m $1 \033[0m "
 }
 
-echoVERT 'Mise a jour du serveur'
-evalLog 'apt-get -y update'
-evalLog 'apt-get -y upgrade'
+echoVERT ':: Mise a jour du serveur'
+apt-get -y update
+apt-get -y upgrade
 
-echoVERT 'Ajout du depot'
-evalLog 'apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common'     
-evalLog 'curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"'
+echoVERT ':: Ajout du depot'
+apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 
-echoVERT 'Installation de Docker-CE'
-evalLog 'apt-get -y update'
-evalLog 'apt-get -y install docker-ce'
+echoVERT ':: Installation de Docker-CE'
+apt-get -y update
+apt-get -y install docker-ce
 
-evalMain 'echo ":: Fin du script" '
+echoVERT ':: Fin du script'
